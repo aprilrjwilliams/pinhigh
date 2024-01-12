0e29faa3791ada9bba037bf2cca2fdd05d94e40e
awilliam_0 <awill051@odu.edu>
2024-01-12 09:16:47 -0600

    footer changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  // standalone: true,
-  // imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
